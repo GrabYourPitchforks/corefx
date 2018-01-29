958e660cacecb09910f32cefc44acf3343a887df
Levi Broderick <levib@microsoft.com>
2018-01-29 13:21:42 -0800

    First naive implementation of NativeLibraryHandle

---
+++ src/System.Runtime.InteropServices/src/System/Runtime/InteropServices/NativeLibraryHandle.Windows.cs
@@ -0,0 +1,62 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
+
+namespace System.Runtime.InteropServices
+{
+    internal sealed partial class NativeLibraryHandle
+    {
+        private static void Load(string name, DllImportSearchPath paths, out IntPtr handle)
+        {
+            // TODO: Cleanup 'paths'
+
+            LoadInternal(name, (uint)paths, out handle);
+
+            if (handle == IntPtr.Zero)
+            {
+                // TODO: Should we fail on specific error codes?
+            }
+        }
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        private static void LoadInternal(string name, uint flags, out IntPtr handle)
+        {
+            handle = NativeMethods.LoadLibraryEx(name, IntPtr.Zero, flags);
+        }
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void Unload()
+        {
+            // No-op if another thread has closed or is in the process of closing this handle
+            if ((InterlockedOr(ref _refCount, HasClosedMask) & HasClosedMask) == 0)
+            {
+                NativeMethods.FreeLibrary(_rawHandle);
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        [SuppressUnmanagedCodeSecurity]
+        private static class NativeMethods
+        {
+            [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool FreeLibrary(
+                [In] IntPtr hModule);
+
+            [DllImport("kernel32.dll", CallingConvention = CallingConvention.Winapi, EntryPoint = "LoadLibraryExW", SetLastError = true)]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+            public static extern IntPtr LoadLibraryEx(
+                [In, MarshalAs(UnmanagedType.LPWStr)] string lpFileName,
+                [In] IntPtr hFile,
+                [In] uint dwFlags);
+        }
+    }
+}
---
+++ src/System.Runtime.InteropServices/src/System/Runtime/InteropServices/NativeLibraryHandle.cs
@@ -0,0 +1,114 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
+
+namespace System.Runtime.InteropServices
+{
+    internal sealed partial class NativeLibraryHandle : CriticalFinalizerObject
+    {
+        private int OneReference = 1 << 2;
+        private int NeedsToCloseMask = 1 << 1;
+        private int HasClosedMask = 1 << 0;
+
+        private readonly IntPtr _rawHandle;
+        private int _refCount;
+
+        public NativeLibraryHandle(string name, DllImportSearchPath paths)
+        {
+            Load(name, paths, out _rawHandle);
+        }
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        ~NativeLibraryHandle()
+        {
+            Unload();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void AddRef(ref bool success)
+        {
+            AddRefInternal(ref success);
+            if (!success)
+            {
+                // TODO: Replace with SR
+                throw new Exception("Handle has been closed.");
+            }
+        }
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void AddRefInternal(ref bool success)
+        {
+            if ((Interlocked.Add(ref _refCount, OneReference) & (NeedsToCloseMask | HasClosedMask)) != 0)
+            {
+                // We just added a reference, but the handle is in the process of being closed, so we need
+                // to release the reference just in case we're blocking final disposal.
+                Release();
+            }
+            else
+            {
+                success = true;
+            }
+        }
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Close()
+        {
+            if ((InterlockedOr(ref _refCount, NeedsToCloseMask) & ~(NeedsToCloseMask | HasClosedMask)) == 0)
+            {
+                // There are no remaining references to this handle. Unload.
+                Unload();
+            }
+        }
+
+        public IntPtr GetRawHandle()
+        {
+            if ((Volatile.Read(ref _refCount) & (NeedsToCloseMask | HasClosedMask)) != 0)
+            {
+                // TODO: replace with SR
+                throw new Exception("Handle has been closed.");
+            }
+            return _rawHandle;
+        }
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int InterlockedOr(ref int location, int value)
+        {
+            var originalValue = Volatile.Read(ref location);
+
+            if ((originalValue & value) != value)
+            {
+                while (true)
+                {
+                    var actualValueBeforeCompXchg = Interlocked.CompareExchange(ref location, originalValue | value, originalValue);
+                    if (actualValueBeforeCompXchg == originalValue)
+                    {
+                        break;
+                    }
+
+                    originalValue = actualValueBeforeCompXchg;
+                }
+            }
+
+            return originalValue;
+        }
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Release()
+        {
+            // Call unload only if this is marked 'needs to close' and we're the last remaining
+            // reference (all ref count bits are clear).
+            if (Interlocked.Add(ref _refCount, -OneReference) == NeedsToCloseMask)
+            {
+                Unload();
+            }
+        }
+    }
+}
