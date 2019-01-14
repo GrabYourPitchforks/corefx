9f276f13e6acc6ced8b20457921098b4e1f41c23
Levi Broderick <levib@microsoft.com>
2019-01-14 15:49:28 -0800

    Add Utf8Tests.ToChars skeleton

---
+++ src/System.Runtime/tests/System/Text/Unicode/Utf8Tests.ToChars.netcoreapp.cs
@@ -0,0 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Buffers;
+using System.Globalization;
+using Xunit;
+
+namespace System.Text.Unicode.Tests
+{
+    public partial class Utf8Tests
+    {
+    }
+}
