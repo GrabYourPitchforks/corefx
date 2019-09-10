6c99ef9695a7ead63e793252710d477d107d21c1
Levi Broderick <levib@microsoft.com>
2019-09-09 17:14:32 -0700

    Add tests for Utf8Span.TryFind{Last}(Rune)

--- src/System.Utf8String.Experimental/tests/System/Utf8SpanTests.Searching.cs
+++ src/System.Utf8String.Experimental/tests/System/Utf8SpanTests.Searching.cs
@@ -41,6 +41,30 @@ namespace System.Text.Tests
 
         [Theory]
         [InlineData("", '\0', null)]
+        [InlineData("Hello", 'l', "2..3")]
+        [InlineData("x\U0001F600y", 0x1F600, "1..5")] // U+1F600 GRINNING FACE
+        [InlineData("x\ud83d\ude00y", 0xF0, null)] // don't match [ F0 ] UTF-8 byte, match only the char
+        [InlineData("x\ud83d\ude00y", 'y', "^1..")]
+        [InlineData("x\u2660\u2661\u2660\u2661y", '\u2660', "1..4")] // U+2660 BLACK SPADE SUIT & U+2661 WHITE HEART SUIT
+        [InlineData("xyz", '\0', null)]
+        [InlineData("xyz\0xyz\0", '\0', "3..4")]
+        public static void TryFind_Rune(string sourceText, uint searchRune, string expectedRange)
+        {
+            using BoundedUtf8Span boundedSpan = new BoundedUtf8Span(sourceText);
+            Utf8Span span = boundedSpan.Span;
+
+            if (span.TryFind(new Rune(searchRune), out Range matchedRange))
+            {
+                AssertRangesEqual(span.Bytes.Length, ParseRangeExpr(expectedRange), matchedRange);
+            }
+            else
+            {
+                Assert.Null(expectedRange);
+            }
+        }
+
+        [Theory]
+        [InlineData("", '\0', null)]
         [InlineData("Hello", 'l', "3..4")]
         [InlineData("x\ud83d\ude00y", '\ud83d', null)] // U+1F600 GRINNING FACE (surrogates should never match)
         [InlineData("x\ud83d\ude00y", 0xF0, null)] // don't match [ F0 ] UTF-8 byte, match only the char
@@ -54,6 +78,30 @@ namespace System.Text.Tests
             Utf8Span span = boundedSpan.Span;
 
             if (span.TryFindLast(searchChar, out Range matchedRange))
+            {
+                AssertRangesEqual(span.Bytes.Length, ParseRangeExpr(expectedRange), matchedRange);
+            }
+            else
+            {
+                Assert.Null(expectedRange);
+            }
+        }
+
+        [Theory]
+        [InlineData("", '\0', null)]
+        [InlineData("Hello", 'l', "3..4")]
+        [InlineData("x\U0001F600y", 0x1F600, "1..5")] // U+1F600 GRINNING FACE
+        [InlineData("x\ud83d\ude00y", 0xF0, null)] // don't match [ F0 ] UTF-8 byte, match only the char
+        [InlineData("x\ud83d\ude00y", 'y', "^1..")]
+        [InlineData("x\u2660\u2661\u2660\u2661y", '\u2660', "7..10")] // U+2660 BLACK SPADE SUIT & U+2661 WHITE HEART SUIT
+        [InlineData("xyz", '\0', null)]
+        [InlineData("xyz\0xyz\0", '\0', "^1..")]
+        public static void TryFindLast_Rune(string sourceText, uint searchRune, string expectedRange)
+        {
+            using BoundedUtf8Span boundedSpan = new BoundedUtf8Span(sourceText);
+            Utf8Span span = boundedSpan.Span;
+
+            if (span.TryFindLast(new Rune(searchRune), out Range matchedRange))
             {
                 AssertRangesEqual(span.Bytes.Length, ParseRangeExpr(expectedRange), matchedRange);
             }
